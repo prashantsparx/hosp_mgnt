d584dd4ddc071340b0df48f204b8fed583df36b8
Prashant Gupta <prashant.gupta@sparxitsolutions.com>
2018-01-26 13:16:39 +0530

    Added Sidemenu Items

---
+++ src/environments/sidemenu.ts
@@ -0,0 +1,679 @@
+export const SideMenuItems = [
+  		{
+  			"id": 1,
+  			"name": "Dashboard",
+  			"url": "/dashboard",
+  			"icon": "dashboard",
+  			"children": []
+  		},
+  		{
+  			"id": 2,
+  			"name": "Masters",
+  			"url": "",
+  			"icon": "work",
+  			"children": [
+  				{
+  					"name": "Supplier",
+		  			"url": "/supplier",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Expense Entry",
+		  			"url": "/expense_entry",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Expense Report",
+		  			"url": "/expense_report",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Expense Main",
+		  			"url": "/expense_main",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Expense Sub",
+		  			"url": "/expense_sub",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Pharmacy Category",
+		  			"url": "/pharmacy_category",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Pharmacy Item",
+		  			"url": "/pharmacy_item",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Pharmacy Units Name",
+		  			"url": "/pharmacy_units_name",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Pharmacy Package Name",
+		  			"url": "/pharmacy_package_name",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Pharmacy Manufacturer Name",
+		  			"url": "/pharmacy_manufacturer_name",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Tax Main",
+		  			"url": "/tax_main",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Tax Sub",
+		  			"url": "/tax_sub",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Bank",
+		  			"url": "/bank",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Credit Card",
+		  			"url": "/credit_card",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Hospital Ward",
+		  			"url": "/hospital_ward",
+		  			"icon": ""
+  				}
+  			]
+  		},
+  		{
+  			"id": 3,
+  			"name": "Hospital",
+  			"url": "",
+  			"icon": "account_balance",
+  			"children": [
+  				{
+  					"name": "Hospital Bill Entry",
+		  			"url": "/hospital_bill_entry",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Print Hospital Invoice",
+		  			"url": "/print_hospital_invoice",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Hospital Invoice Report",
+		  			"url": "/hospital_invoice_report",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Hospital Statement - By Patient",
+		  			"url": "/hospital_statement_patient",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Hospital Statement - By Invoice",
+		  			"url": "/hospital_statement_invoice",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Hospital Sales Report By Category",
+		  			"url": "/hospital_sales_report_category",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Final Bill By Item",
+		  			"url": "/final_bill_item",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Final Discount Bill By Item",
+		  			"url": "/final_discount_bill_item",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Final Bill By Date",
+		  			"url": "/final_bill_date",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Final Discount Bill By Date",
+		  			"url": "/final_discount_bill_date",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Hospital Collection Entry",
+		  			"url": "/hospital_collection_entry",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Hospital Collection Received",
+		  			"url": "/hospital_collection_received",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Hospital Collection Pending",
+		  			"url": "/hospital_collection_pending",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Hospital Advance Report",
+		  			"url": "/hospital_advance_report",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Patient Discharge",
+		  			"url": "/patient_discharge",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Discharge Summary Report",
+		  			"url": "/discharge_summary_report",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Create Hospital Item",
+		  			"url": "/create_hospital_item",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Create Hospital Category",
+		  			"url": "/create_hospital_category",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Hospital Package Name",
+		  			"url": "/hospital_package_name",
+		  			"icon": ""
+  				}
+  			]
+  		},
+  		{
+  			"id": 4,
+  			"name": "Doctors",
+  			"url": "",
+  			"icon": "group_add",
+  			"children": [
+  				{
+  					"name": "Doctor Entry",
+		  			"url": "/doctor_entry",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Doctor List",
+		  			"url": "/doctor_list",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Doctor Address List",
+		  			"url": "/doctor_address_list",
+		  			"icon": ""
+  				}
+  			]
+  		},
+  		{
+  			"id": 5,
+  			"name": "Patients",
+  			"url": "",
+  			"icon": "accessible",
+  			"children": [
+  				{
+  					"name": "Patient Registration",
+		  			"url": "/patient_registration",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Patient List",
+		  			"url": "/patient_list",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Patient Address List",
+		  			"url": "/patient_address_list",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Patient Admission",
+		  			"url": "/patient_admission",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Patient Admitted List",
+		  			"url": "/patient_admitted_list",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Patient Discharged List",
+		  			"url": "/patient_discharged_list",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Prescription Entry",
+		  			"url": "/prescription_entry",
+		  			"icon": ""
+  				},
+  				{
+  					"name": "Prescription Report",
+		  			"url": "/prescription_report",
+		  			"icon": ""
+  				}
+  			]
+  		},
+  		{
+  			"id": 6,
+  			"name": "Lab",
+  			"url": "",
+  			"icon": "assignment_turned_in",
+  			"children": [
+          {
+            "name": "Lab Invoice Entry",
+            "url": "/lab_invoice_entry",
+            "icon": ""
+          },
+          {
+            "name": "Lab Category",
+            "url": "/lab_category",
+            "icon": ""
+          },
+          {
+            "name": "Lab Item",
+            "url": "/lab_item",
+            "icon": ""
+          },
+          {
+            "name": "Lab Collection Entry",
+            "url": "/lab_collection_entry",
+            "icon": ""
+          },
+          {
+            "name": "Lab Invoice Report",
+            "url": "/lab_invoice_report",
+            "icon": ""
+          },
+          {
+            "name": "Lab Statement - By Patient",
+            "url": "/lab_statement_patient",
+            "icon": ""
+          },
+          {
+            "name": "Lab Statement - By Invoice",
+            "url": "/lab_statement_invoice",
+            "icon": ""
+          },
+          {
+            "name": "Lab Collection Pending",
+            "url": "/lab_collection_pending",
+            "icon": ""
+          },
+          {
+            "name": "Lab Collection Received",
+            "url": "/lab_collection_received",
+            "icon": ""
+          },
+          {
+            "name": "Lab Test Category",
+            "url": "/lab_test_category",
+            "icon": ""
+          },
+          {
+            "name": "Lab Test Item",
+            "url": "/lab_test_item",
+            "icon": ""
+          },
+          {
+            "name": "Lab Test Report Entry",
+            "url": "/lab_test_report_entry",
+            "icon": ""
+          },
+          {
+            "name": "Search Patients - Lab",
+            "url": "/search_lab_patients",
+            "icon": ""
+          },
+          {
+            "name": "Lab Test Report List",
+            "url": "/lab_test_report_list",
+            "icon": ""
+          },
+          {
+            "name": "Lab Units Name",
+            "url": "/lab_units_name",
+            "icon": ""
+          }
+        ]
+  		},
+  		{
+  			"id": 7,
+  			"name": "Pharma In",
+  			"url": "",
+  			"icon": "add_shopping_cart",
+  			"children": [
+          {
+            "name": "Purchase Entry",
+            "url": "/purchase_entry",
+            "icon": ""
+          },
+          {
+            "name": "Purchase Return Entry",
+            "url": "/purchase_return_entry",
+            "icon": ""
+          },
+          {
+            "name": "Supplier Payment Entry",
+            "url": "/supplier_payment_entry",
+            "icon": ""
+          },
+          {
+            "name": "Purchase Report - By Bill",
+            "url": "/purchase_report_bill",
+            "icon": ""
+          },
+          {
+            "name": "Purchase Report - By Supplier",
+            "url": "/purchase_report_supplier",
+            "icon": ""
+          },
+          {
+            "name": "Purchase Return Report",
+            "url": "/purchase_return_report",
+            "icon": ""
+          },
+          {
+            "name": "Supplier Payment Due Report",
+            "url": "/supplier_due_payment_report",
+            "icon": ""
+          },
+          {
+            "name": "Supplier Payment Report",
+            "url": "/supplier_payment_report",
+            "icon": ""
+          },
+          {
+            "name": "VAT Input Report",
+            "url": "/vat_input_report",
+            "icon": ""
+          },
+          {
+            "name": "Statement By Purchase",
+            "url": "/statement_by_purchase",
+            "icon": ""
+          },
+          {
+            "name": "Print Purchase Bill",
+            "url": "/print_purchase_bill",
+            "icon": ""
+          },
+          {
+            "name": "Print Purchase Return",
+            "url": "/print_purchase_return",
+            "icon": ""
+          },
+          {
+            "name": "Purchase Report - By Item",
+            "url": "/purchase_report_item",
+            "icon": ""
+          }
+        ]
+  		},
+  		{
+  			"id": 8,
+  			"name": "Pharma Out",
+  			"url": "",
+  			"icon": "local_shipping",
+  			"children": [
+          {
+            "name": "Sales Bill Entry",
+            "url": "/sales_bill_entry",
+            "icon": ""
+          },
+          {
+            "name": "Sales Return Entry",
+            "url": "/sales_return_entry",
+            "icon": ""
+          },
+          {
+            "name": "Sales Collection Entry",
+            "url": "/sales_collection_entry",
+            "icon": ""
+          },
+          {
+            "name": "Sales Report - By Bill",
+            "url": "/sales_report_bill",
+            "icon": ""
+          },
+          {
+            "name": "Sales Report - By Patient",
+            "url": "/sales_report_patient",
+            "icon": ""
+          },
+          {
+            "name": "Sales Return Report",
+            "url": "/sales_return_report",
+            "icon": ""
+          },
+          {
+            "name": "Collection Received Report",
+            "url": "/collection_received_report",
+            "icon": ""
+          },
+          {
+            "name": "Collection Pending Report",
+            "url": "/collection_pending_report",
+            "icon": ""
+          },
+          {
+            "name": "VAT Output Report",
+            "url": "/vat_output_report",
+            "icon": ""
+          },
+          {
+            "name": "VAT Payable Report",
+            "url": "/vat_payable_report",
+            "icon": ""
+          },
+          {
+            "name": "Print Sales Bill",
+            "url": "/print_sales_bill",
+            "icon": ""
+          },
+          {
+            "name": "Print Sales Return",
+            "url": "/print_sales_return",
+            "icon": ""
+          },
+          {
+            "name": "Discount Given Report",
+            "url": "/discount_given_report",
+            "icon": ""
+          },
+          {
+            "name": "Edited Bill Report",
+            "url": "/edited_bill_report",
+            "icon": ""
+          },
+          {
+            "name": "Sales Margin Report",
+            "url": "/sales_margin_report",
+            "icon": ""
+          }
+        ]
+  		},
+  		{
+  			"id": 9,
+  			"name": "Stock",
+  			"url": "",
+  			"icon": "store",
+  			"children": [
+          {
+            "name": "Stock Adjustment",
+            "url": "/stock_adjustment",
+            "icon": ""
+          },
+          {
+            "name": "Stock Report - By Date",
+            "url": "/stock_report_date",
+            "icon": ""
+          },
+          {
+            "name": "Stock Report - By Item",
+            "url": "/stock_report_item",
+            "icon": ""
+          },
+          {
+            "name": "Stock Report - By Category",
+            "url": "/stock_report_category",
+            "icon": ""
+          },
+          {
+            "name": "Closing Stock Reporr",
+            "url": "/closing_stock_report",
+            "icon": ""
+          },
+          {
+            "name": "Stock Movement Report",
+            "url": "/stock_movement_report",
+            "icon": ""
+          },
+          {
+            "name": "Expiry Stock Report",
+            "url": "/expiry_stock_report",
+            "icon": ""
+          },
+          {
+            "name": "Stock Adjustment Report",
+            "url": "/stock_adjustment_report",
+            "icon": ""
+          },
+          {
+            "name": "Schedule H Report",
+            "url": "/schedule_h_report",
+            "icon": ""
+          },
+          {
+            "name": "Non Moving Items",
+            "url": "/non_moving_items",
+            "icon": ""
+          },
+          {
+            "name": "Fast/Slow Moving Items",
+            "url": "/fast_slow_moving_items",
+            "icon": ""
+          },
+          {
+            "name": "Drug Destroyed Entry",
+            "url": "/drug_destroyed_entry",
+            "icon": ""
+          },
+          {
+            "name": "Drug Destroyed Report",
+            "url": "/drug_destroyed_report",
+            "icon": ""
+          }
+        ]
+  		},
+  		{
+  			"id": 10,
+  			"name": "Settings",
+  			"url": "",
+  			"icon": "settings",
+  			"children": [
+          {
+            "name": "Wishes Printing",
+            "url": "/wishes_printing",
+            "icon": ""
+          },
+          {
+            "name": "Print Out Copy Main Master",
+            "url": "/print_out_copy_master",
+            "icon": ""
+          },
+          {
+            "name": "New Employee Add",
+            "url": "/new_employee_add",
+            "icon": ""
+          },
+          {
+            "name": "Employee Edit",
+            "url": "/employee_edit",
+            "icon": ""
+          },
+          {
+            "name": "Printer Settings",
+            "url": "/printer_settings",
+            "icon": ""
+          },
+          {
+            "name": "Settings Hospital Bill",
+            "url": "/settings_hospital_bill",
+            "icon": ""
+          },
+          {
+            "name": "Settings Purchase Bill",
+            "url": "/settings_purchase_bill",
+            "icon": ""
+          },
+          {
+            "name": "Logo Upload",
+            "url": "/logo_upload",
+            "icon": ""
+          },
+          {
+            "name": "Bill Type Order Change",
+            "url": "/bill_type_order_change",
+            "icon": ""
+          },
+          {
+            "name": "Round Off Settings",
+            "url": "/round_off_settings",
+            "icon": ""
+          },
+          {
+            "name": "Print Out Title Name Master",
+            "url": "/print_out_title_master",
+            "icon": ""
+          },
+          {
+            "name": "Settings Master",
+            "url": "/settings_master",
+            "icon": ""
+          },
+          {
+            "name": "Settings Pharmacy Bill",
+            "url": "/settings_pharmacy_bill",
+            "icon": ""
+          },
+          {
+            "name": "Settings Lab Bill",
+            "url": "/settings_lab_bill",
+            "icon": ""
+          },
+          {
+            "name": "Settings Prescription",
+            "url": "/settings_prescription",
+            "icon": ""
+          },
+          {
+            "name": "Settings Lab Test",
+            "url": "/settings_lab_test",
+            "icon": ""
+          }
+        ]
+  		},
+  		{
+  			"id": 11,
+  			"name": "Utility",
+  			"url": "",
+  			"icon": "vpn_key",
+  			"children": [
+          {
+            "name": "Live Support",
+            "url": "/live_support",
+            "icon": ""
+          }
+        ]
+  		},
+  	];
